cdb48aaf800b71dddd895c6fece2ba14829733f2
Tasos K <t.kozikopoulos@gmail.com>
2019-06-16 02:18:40 +0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,7 +1,7 @@
 """
 Follow these steps to acquire the necessary credentials and fill them in:
     1. Log in to Twitter and make sure you have verified your phone number, otherwise you
-       won't be able to create an Twitter app.
+       won't be able to create a Twitter app.
     2. Go to apps.twitter.com.
     3. Click "Create New App" and fill in the necessary fields.
     4. Go to the "Keys and Access Tokens" tab and copy/paste the first 2 credentials.
@@ -11,8 +11,8 @@ Follow these steps to acquire the necessary credentials and fill them in:
 """
 
 # Variables for using the Twitter API.
-# These are not valid, but they are the format yours will most likely be in so you know what to expect.
-# NOTE: Make sure you don't share them with anyone!
+# These are not valid, but they are the format yours will most likely be in, so you know what to expect.
+# NOTE: Make sure you don't share these keys with with anyone!
 CONSUMER_KEY = 'MHQ92pJSD80dkjrPn0G7SZBJI'
 CONSUMER_SECRET = 'OTgIxbmZF8gf52dQCUDZFKATj9wnNHGjCVzwC2Nnbrc7yuo7M'
 
